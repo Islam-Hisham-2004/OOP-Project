247c6ab95d124cd0e84f92845d9f4be643b8c033
KeroNagy25 <kerokarkar2003@gmail.com>
2023-12-08 01:01:34 +0200

    comments and replies

---
+++ Reply.java
@@ -0,0 +1,42 @@
+package javaapplication7;
+
+import java.time.LocalDateTime;
+
+public class Reply {
+
+    private int replyId;
+    private String userName;
+    private String content;
+    private LocalDateTime time;
+
+    public Reply(int replyId, String userName, String content) {
+        this.replyId = replyId;
+        this.userName = userName;
+        this.content = content;
+        this.time = LocalDateTime.now();
+    }
+
+    public int getReplyId() {
+        return replyId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Reply{" + "replyId=" + replyId + ", userName=" + userName + ", content=" + content + ", time=" + time + '}';
+    }
+    
+    
+
+}
---
+++ comment.java
@@ -0,0 +1,56 @@
+package javaapplication7;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class comment {
+
+    private int commentId;
+    private String userName;
+    private String content;
+    private LocalDateTime time;
+    private List<Reply> replies;
+
+    public comment(int commentId, String userName, String content) {
+        this.commentId = commentId;
+        this.userName = userName;
+        this.content = content;
+        this.time = LocalDateTime.now();
+        this.replies = new ArrayList<>();
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public List<Reply> getReplies() {
+        return replies;
+    }
+
+    public void addReply(Reply reply) {
+        replies.add(reply);
+    }
+
+    @Override
+    public String toString() {
+        return "comment{" + "commentId=" + commentId + ", userName=" + userName + ", content=" + content + ", time=" + time + ", replies=" + replies + '}';
+    }
+    
+     
+    
+    
+
+}
