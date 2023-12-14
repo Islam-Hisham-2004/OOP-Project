0f917c9fb9b4edad1484958356c03148c0b05807
MalakMocha <mickywaleed2004@gmail.com>
2023-12-14 14:15:49 +0200

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,62 +1,71 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+import java.util.Calendar;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.io.FileWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public  class User {
-    private static int userID = 0;
-    private String Firstname;
-    private String Lastname;
+interface AccountManagement {
+    User createAccount(String firstName, String lastName, String username, String password,
+                       String confirmPassword, String email, Date birthdate, String profilePictureUrl);
+
+    boolean login(String username, String password);
+}
+
+public class User implements AccountManagement {
+    private UUID userID;
+    private String firstName;
+    private String lastName;
     private String email;
     private String username;
     private String password;
-    private String Confirmpassword;
-    private static String profilePictureUrl=" "; //4aklaha m4 ht4t8all nnn
+    private String confirmPassword;
+    private String profilePictureUrl = " "; // Default value
     private Date birthdate;
-    // private ArrayList<String> ListOfFriends = new ArrayList<String>();
-    List<User> ListOfFriends;
-    private ArrayList followedPages= new ArrayList<>();
-    private User[] friends;
+    private List<User> listOfFriends;
+    private Gender gender;
+    private String Confirmpassword;
 
-
-    public User(int userID, String Firstname,String Lastname, String email, String username, String password, String Confirmpassword )
-    {
-        userID ++;
-        this.Firstname = Firstname;
-        this.Lastname = Lastname;
+    public User(String firstName, String lastName, String email, String username,
+                String password, String confirmPassword, Gender gender) {
+        this.userID = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.username = username;
         this.password = password;
-        this.Confirmpassword = Confirmpassword;
-        this.profilePictureUrl = profilePictureUrl;
-        this.birthdate = birthdate;
-        ArrayList<String> listOfPosts = new ArrayList<>();
-        this.ListOfFriends = new ArrayList<>();
-        ArrayList<String> listOfConversations = new ArrayList<>();
-        this.followedPages = new ArrayList<>();
+        this.confirmPassword = confirmPassword;
+        this.gender = gender;
     }
 
-    public int getUserID() {
+    public UUID getUserID() {
         return userID;
     }
 
-    public void setUserID(int userID) {
+    public void setUserID(UUID userID) {
         this.userID = userID;
     }
 
-    public String getFirstname() {
-        return Firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirstname(String Firstname) {
-        this.Firstname = Firstname;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLastname() {
-        return Lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLastname(String Lastname) {
-        this.Lastname = Lastname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -74,6 +83,7 @@ public  class User {
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
@@ -82,12 +92,12 @@ public  class User {
         this.password = password;
     }
 
-    public String getConfirmpassword() {
-        return Confirmpassword;
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 
-    public void setConfirmpassword(String Confirmpassword) {
-        this.Confirmpassword = Confirmpassword;
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 
     public String getProfilePictureUrl() {
@@ -98,10 +108,6 @@ public  class User {
         this.profilePictureUrl = profilePictureUrl;
     }
 
-    /*public ArrayList<String> getListOfFriends() {
-        return ListOfFriends;
-    }*/
-
     public Date getBirthdate() {
         return birthdate;
     }
@@ -110,30 +116,164 @@ public  class User {
         this.birthdate = birthdate;
     }
 
-    /*public void setListOfFriends(ArrayList<String> listOfFriends) {
-        ListOfFriends = listOfFriends;
-    }*/
-    /*public void addFriend(User friend) {
-        friends.add(friend);
+    public List<User> getListOfFriends() {
+        return listOfFriends;
     }
 
-    public void removeFriend(User friend) {
-        friends.remove(friend);
-    }*/
-    public void deletePost(Post post) {
-        posts.remove(post);
+    public void setListOfFriends(List<User> listOfFriends) {
+        this.listOfFriends = listOfFriends;
     }
 
-    public static User createAccount(String Firstname, String Lastname, String username, String password, String email, Date birthdate){
-        if (username == null || password == null || Firstname == null || email == null || profilePictureUrl == null) {
-            throw new IllegalArgumentException("All parameters are required");
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getConfirmpassword() {
+        return Confirmpassword;
+    }
+
+    public void setConfirmpassword(String confirmpassword) {
+        Confirmpassword = confirmpassword;
+    }
+
+    public User(UUID userID, String username, String firstName, String lastName,
+                String password, String confirmPassword, String email, Date birthdate,
+                String profilePictureUrl, Gender gender) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.profilePictureUrl = profilePictureUrl;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    // Implementing methods from AccountManagement interface
+
+    @Override
+    public User createAccount(String Firstname, String Lastname, String username, String password, String confirmpassword, String email, Date birthdate, String profilePictureUrl){
+        if (isValidUsername(username) && isValidPassword(password, confirmpassword) &&
+                isValidEmail(email) && isValidBirthdate(birthdate) && isValidProfilePictureUrl(profilePictureUrl)) {
+            this.username = username;
+            this.password = password;
+            this.Confirmpassword = confirmpassword;
+
+            this.email = email;
+            this.birthdate = birthdate;
+            this.profilePictureUrl = profilePictureUrl;
+            this.userID = UUID.randomUUID(); // Generate a random and unique UUID
+        } else {
+            throw new IllegalArgumentException("Invalid account details");
         }
-        User user = new User(userID, username, password, Firstname, Lastname, email, profilePictureUrl);
-        User user1 = user;
-        return user1;
 
+        if (isValidEmail(email)) {
+            this.email = email;
+        } else throw new IllegalArgumentException("Invalid email format");
 
+        //return user1;
+        return null;
     }
+
+    private boolean isValidUsername(String username) {
+        /* check for a minimum length requirement */
+        return username != null && username.length() >= 4;
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        /* check for length, complexity, matching confirmation */
+        return password != null && password.length() >= 6 && password.equals(confirmPassword);
+    }
+
+    private boolean isValidBirthdate(Date birthdate) {
+        if (birthdate == null) {
+            return false;
+        }
+        // Get the current date
+        Calendar currentDate = Calendar.getInstance();
+
+        // Check if the birthdate is in the future
+        if (birthdate.after(currentDate.getTime())) {
+            return false;
+        }
+
+        // Check if the birthdate is within a reasonable range (e.g., not too far in the past)
+        Calendar reasonableRange = Calendar.getInstance();
+        reasonableRange.add(Calendar.YEAR, -150); //ay range zy m na 3aiza
+
+        return birthdate.after(reasonableRange.getTime());
+    }
+
+    private boolean isValidProfilePictureUrl(String profilePictureUrl) {
+        // For example, check if it's a valid URL
+        return profilePictureUrl != null && profilePictureUrl.startsWith("http");
+    }
+
+
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        /*ensures that the email:
+        Starts with one or more alphanumeric characters, underscores, plus signs, ampersands, asterisks, or hyphens.
+        Contains an '@' symbol.
+        Followed by one or more groups of characters separated by dots, where each group starts with alphanumeric characters, underscores, plus signs, ampersands, asterisks, or hyphens.
+        Ends with a dot and two to seven alphabetic characters.*/
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        // creates a Pattern object and compiles the regular expression emailRegex into the Pattern object.
+
+        Matcher matcher = pattern.matcher(email);
+        //creates a Matcher object (matcher) for the specified email address (email).
+
+
+        // Return true if the email matches the pattern
+        return matcher.matches();
+    }
+
+   /* private static List<User> readUsersFromFile(String filePath) {
+        String defaultEmail = "unknown@example.com";
+        List<User> users = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String savedUsername = parts[0];
+                    String savedPassword = parts[1];
+
+                    // Default or placeholder values for optional attributes
+                    String defaultFirstName = "Unknown";
+                    String defaultLastName = "Unknown";
+                    Date defaultBirthdate = new Date(); // Provide a default date
+                    String defaultProfilePictureUrl = "default.jpg";
+                    UUID defaultUserID = UUID.randomUUID(); // Generate a default UUID
+
+                    User user = new User(savedUsername, savedPassword,
+                            defaultFirstName, defaultLastName, defaultEmail,
+                            defaultBirthdate, defaultProfilePictureUrl, defaultUserID);
+
+                    users.add(user);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }*/
+
+
+
+
+    @Override
     public boolean login(String username, String password) {
         if (username == null || username.isEmpty()) {
             throw new IllegalArgumentException("Username cannot be null or empty");
@@ -143,88 +283,6 @@ public  class User {
         }
         return this.username.equals(username) && this.password.equals(password);
     }
-    public Post writePost(String title, String content) {
 
-        if (title == null || title.isEmpty()) {
-            throw new IllegalArgumentException("Title cannot be null or empty");
-        }
-        if (content == null || content.isEmpty()) {
-            throw new IllegalArgumentException("Content cannot be null or empty");
-        }
-
-        Post newPost = new Post(title, content, this); //this represents the user who is creating the post
-        this.posts.add(newPost);
-        return newPost;
-    }
-    public void writeComments(Post post, String commentText) {
-        Comment comment = new Comment(this, commentText);  //this represents the user who is commenting on the post
-        post.addComment(comment);
-    }
-    public void likeComment(Comment comment) {
-        comment.addLike(this); //add user to the list of likes (this represents the user)
-    }
-    /*public void likePost(Post post) {
-        post.addLike(this);
-    }*/
-    public void seeFriendsPosts() {
-        // User[] friendList;
-        if (ListOfFriends.isEmpty()) {
-            System.out.println("You have no friends to see posts from.");
-            return;
-        }
-
-        System.out.println("Posts from friends:");
-
-        for (User friend : ListOfFriends) {
-            96            System.out.println("Posts from " + friend.getUsername() + ":");
-            List<Post> friendPosts = friend.getPosts();
-
-            if (friendPosts.isEmpty()) {
-                System.out.println(" - No posts from " + friend.getUsername());
-            } else {
-                for (Post friendPost : friendPosts) {
-                    System.out.println(" - " + friendPost.getMessage());
-                }
-            }
-
-            System.out.println(); // Separate friend posts
-        }
-    }
-
-    //public Page createPage();
-
-    public void searchAddFriends(User friend) {
-        if (ListOfFriends.contains(friend) || friend.equals(this)) {
-            System.out.println("User not found or already a friend.");
-        } else {
-            ListOfFriends.add(friend);
-            friend.ListOfFriends.add(this); // Add reciprocal friendship
-            System.out.println("Friend added: " + friend.getUsername());
-        }
-    }
-    public void followPage(Page page) {
-        if (!this.followedPages.contains(page)) {
-            this.followedPages.add(page);
-        }
-    }
-
-    public List<Page> getFollowedPages() {
-        return this.followedPages;
-    }
-
-
-
-
-    public List<User> getMutualFriends(User otherUser) {
-        List<User> mutualFriends = new ArrayList<>();
-
-        for (User friend : this.friends) {
-            if (otherUser.getFriends().contains(friend)) {
-                mutualFriends.add(friend);
-            }
-        }
-
-        return mutualFriends;
-    }
-
+    
 }
